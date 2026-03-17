12383eb3c73f1718a025ae4f2192b7afd927f230
javier.turcios <ulricjavier@gmail.com>
2026-03-17 12:40:53 +0000

    feat: agregar clases Cliente y Fraccion con métodos básicos

---
+++ entregas/turciosJavier/src/reto-004/Cliente.java
@@ -0,0 +1,5 @@
+package vistas.fraccion
+
+public class Cliente{
+    
+}
---
+++ entregas/turciosJavier/src/reto-004/Fraccion.java
@@ -0,0 +1,26 @@
+package vistas.fraccion;
+
+public class Fraccion{
+    public Fraccion(int numerador, int denominador){}
+    public Fraccion(int numero){}
+    public Fraccion(Fraccion fraccion){}
+    public Fraccion(){}
+
+    public Fraccion clonar(){}
+
+    public void sumar(Fraccion fraccion){} 
+    public void restar(Fraccion fraccion){} 
+    public void multiplicar(Fraccion fraccion){} 
+    public void dividir(Fraccion fraccion){} 
+
+    public boolean esMayor(Fraccion fraccion){}
+    public boolean esMenor(Fraccion fraccion){}
+
+    public boolean equals(Fraccion fraccion){}
+
+    public void mostrar(){}
+    public void invertir(){}
+    
+
+
+}
