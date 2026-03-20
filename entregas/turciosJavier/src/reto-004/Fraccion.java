8f56ff62277428749bfb228646849558b470b65a
javier.turcios <ulricjavier@gmail.com>
2026-03-20 09:40:13 +0000

    feat: agregar calcularMCD y constructor principal con normalización

--- entregas/turciosJavier/src/reto-004/Fraccion.java
+++ entregas/turciosJavier/src/reto-004/Fraccion.java
@@ -9,7 +9,31 @@ public class Fraccion{
         this.denominador = 1;
     }
 
-    public Fraccion clonar(){}
+    public Fraccion(int numerador, int denominador) {
+        if (denominador == 0) {
+            denominador = 1;
+        }
+        
+        if (denominador < 0) {
+            numerador = -numerador;
+            denominador = -denominador;
+        }
+
+        int mcd = calcularMCD(numerador, denominador);
+        this.numerador = numerador / mcd;
+        this.denominador = denominador / mcd;
+    }
+
+    private int calcularMCD(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        while (b != 0) {
+            int temporal = b;
+            b = a % b;
+            a = temporal;
+        }
+        return a;
+    }
 
     public void sumar(Fraccion fraccion){} 
     public void restar(Fraccion fraccion){} 
